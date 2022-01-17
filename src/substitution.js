010b594539087679d8c6a54da82cde885c5de688
Misato Saarna <misatosaarna@gmail.com>
2022-01-16 19:20:43 -0600

    added a space

--- src/substitution.js
+++ src/substitution.js
@@ -19,6 +19,7 @@ const substitutionModule = (function () {
       }
     };
 
+    
     let answer = '';
     let lowerCased = input.toLowerCase();
     const alphabet1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
